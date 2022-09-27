0c2fd7ad6805ca7dda18e73c744a9d92ca7a7f3a
willallstet <willallstetter@gmail.com>
2022-09-27 17:27:31 -0400

    first commit

--- app.js
+++ app.js
@@ -26,14 +26,26 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  //console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
   // TODO: Create a new HTML element to display your data
 
+  const whichElement = Math.floor(Math.random() * fruitsArray.length);
+  const newElement = document.createElement('p');
+  const order = fruitsArray[whichElement].order;
+  const result = fruitsArray.filter(fruit => fruit.order == order);
+  newElement.innerHTML = "Fruits of the order " + order + ": ";
+  result.forEach(element => newElement.innerHTML = newElement.innerHTML + element.name + ', ');
+
+  // Append the new element to an existing part of the webpage
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newElement);
+
   // TODO: Append your new element to the page
+  window.scrollTo(0, document.body.scrollHeight);
 
 }
 
